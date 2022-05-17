74cc6ae3e44c9794d950b5b24e3c26a7a5f8c0ac
Proguc <45713637+Proguc@users.noreply.github.com>
2022-05-17 05:57:39 +0300

    zero

--- venv/parser.py
+++ venv/parser.py
@@ -9,7 +9,7 @@ class Data:
     def __init__(self):
         self.URL = "https://slovopoisk.ru/type-1/length-5?page="
 
-    def pars(self):
+    def createBD(self):
         bd = []
         pages = (9576 / 100)
         for i in range(1, math.ceil(pages) + 1):
@@ -20,21 +20,11 @@ class Data:
                 num = element.find("span", class_="word-num")
                 s = str(element.text.strip())
                 s1 = str(num.text.strip())
-                print(s.replace(s1, "").strip())
-                bd.append(s.replace(s1, "").strip())
+                bd.append(s.replace(s1, "").strip().lower())
 
         my_df = pd.DataFrame(bd)
         my_df.to_csv('bd.csv', index=False, header=False)
-
-    @staticmethod
-    def updateBD(self):
-        pars()
         print("The database has been created")
-
-    @staticmethod
-    def createBD(self):
-        pars()
-        print("The database has been updated")
 
     @staticmethod
     def addData():
